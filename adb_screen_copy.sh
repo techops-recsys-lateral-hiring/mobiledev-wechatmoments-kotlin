fb99342f1679409a4bae0c7c90296fba731f4d06
kayel <kayel.yuri@gmail.com>
2023-06-08 06:12:20 -0300

    AGP 8 API 33

---
+++ adb_screen_copy.sh
@@ -0,0 +1,36 @@
+scrcpy --turn-screen-off --show-touches
+
+
+# ADB Screen Copy
+#
+## Scrcpy
+#Pronounced as "screen copy," allows you to mirror the screen of an Android device on your computer and control it using your keyboard and mouse. It is a free and open-source software developed by Genymobile.
+#
+#Scrcpy offers several features and benefits, including:
+#
+#- **Lightness**: It is a native application that only displays the device screen, ensuring a lightweight performance.
+#- **Performance**: Scrcpy achieves a high frame rate ranging from 30 to 120fps, depending on the connected device.
+#- **Quality**: The mirrored screen has a resolution of 1920x1080 or higher, ensuring a high-quality display.
+#- **Low latency**: Scrcpy provides low latency between the device and the computer, typically ranging from 35 to 70ms.
+#- **Low startup time**: It takes approximately one second to display the first image after launching the application.
+#- **Non-intrusiveness**: Scrcpy does not leave any installation traces on the Android device.
+#- **User benefits**: The application does not require user accounts, displays no ads, and does not need an internet connection.
+#- **Freedom**: Scrcpy is free and open-source software, allowing users to modify and customize it according to their needs.
+#
+#To use scrcpy, you need to install it on your computer and connect your Android device via USB or over TCP/IP. Once connected, you can control your Android device's screen using your computer's keyboard and mouse.
+#
+#Scrcpy supports multiple operating systems, including Linux, Windows, and macOS. It offers additional features such as audio forwarding (available on Android devices running Android 11 or later) and support for video codecs like H265 and AV1.
+#
+#If you're interested in downloading scrcpy or learning more about its usage, you can visit the following resources:
+#
+#1. [GitHub repository for scrcpy](https://github.com/Genymobile/scrcpy) [[🔗](https://github.com/Genymobile/scrcpy)]
+#2. [Official website for scrcpy](https://scrcpy.org/) (Note: The website seems to be unreachable at the moment.)
+#
+#You can also refer to tutorials and guides available on various platforms, such as:
+#
+#- [Tom's Hardware guide on controlling Android devices using scrcpy](https://www.tomshardware.com/how-to/control-android-device-from-pc-using-scrcpy) [[🔗](https://www.tomshardware.com/how-to/control-android-device-from-pc-using-scrcpy)]
+#- [Wikipedia page on scrcpy](https://en.wikipedia.org/wiki/Scrcpy) [[🔗](https://en.wikipedia.org/wiki/Scrcpy)]
+#- [XDA Developers article on scrcpy and its features](https://www.xda-developers.com/free-android-mirroring-app-scrcpy-seamless-copy-paste-stay-awake/) [[🔗](https://www.xda-developers.com/free-android-mirroring-app-scrcpy-seamless-copy-paste-stay-awake/)]
+#- [GeeksforGeeks tutorial on mirroring Android screen to PC using scrcpy](https://www.geeksforgeeks.org/mirror-android-screen-to-pc-using-scrcpy/) [[🔗](https://www.geeksforgeeks.org/mirror-android-screen-to-pc-using-scrcpy/)]
+#- [Softonic page for downloading scrcpy](https://scrcpy.en.softonic.com/) [[🔗](https://scrcpy.en.softonic.com/)]
+#- [Tecmint guide on using scrcpy to control Android devices via Linux desktop](https://www.tecmint.com/scrcpy-connect-android-linux/) [[🔗](https://www.tecmint.com/scrcpy-connect-android-linux/)]
